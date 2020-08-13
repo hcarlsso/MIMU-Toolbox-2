16b1263138371d51dc0656aa3ccb180ab0a566a9
Håkan Carlsson <hakcar@UG.KTH.SE>
2020-08-13 14:31:13 +0200

    Fix acc scale factor

--- +mimu_read_out/scale_measurements.m
+++ +mimu_read_out/scale_measurements.m
@@ -1,12 +1,19 @@
 function inertial_data_scaled = scale_measurements(inertial_data, varargin)
-%scale_raw_acc Scale raw_acc to m/s^2
-
+% scale_measurements scale_raw_acc Scale raw_acc to m/s^2
+% Sensitivity Scale Factor
+% AFS_SEL=0     16,384 LSB/g
+% AFS_SEL=1     8,192  LSB/g
+% AFS_SEL=2     4,096  LSB/g
+% AFS_SEL=3     2,048LSB/g
     p = inputParser;
     
-    addParameter(p, 'scale_acc', 2048*9.80665, @(x) x > 0);
+    addParameter(p, 'scale_acc', 2048/9.80665, @(x) x > 0);
     addParameter(p, 'scale_gyro', 16.4, @(x) x > 0);
     
     parse(p,varargin{:});
+    
+    fprintf("Scale accelerometer: %.2e", p.Results.scale_acc)
+    fprintf("Scale gyroscope: %.2e", p.Results.scale_gyro)
     
     N_sensors = size(inertial_data, 1);
     inds_acc = sort([1:6:N_sensors, 2:6:N_sensors, 3:6:N_sensors]);
